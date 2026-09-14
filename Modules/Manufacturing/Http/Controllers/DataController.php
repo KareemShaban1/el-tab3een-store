5138a38d000a431b8109783ce61edd27304f3dc1
KareemShaban1 <karim17196@fci.bu.edu.eg>
2026-09-14 21:20:18 +0300

    handle manufacturing link issue

--- Modules/Manufacturing/Http/Controllers/DataController.php
+++ Modules/Manufacturing/Http/Controllers/DataController.php
@@ -137,9 +137,8 @@ class DataController extends Controller
         $is_mfg_enabled = auth()->user()->can('superadmin')
             || (boolean) $module_util->hasThePermissionInSubscription($business_id, 'manufacturing_module', 'superadmin_package');
 
-//         if ($is_mfg_enabled && (auth()->user()->can('manufacturing.access_recipe') || auth()->user()->can('manufacturing.access_production'))) {
                   if ($is_mfg_enabled ) {  
-	Menu::modify('admin-sidebar-menu', function ($menu) use ($business_id) {
+		Menu::modify('admin-sidebar-menu', function ($menu) use ($business_id) {
                 $menu->dropdown(
                     __('manufacturing::lang.manufacturing'),
                     function ($sub) use ($business_id) {
